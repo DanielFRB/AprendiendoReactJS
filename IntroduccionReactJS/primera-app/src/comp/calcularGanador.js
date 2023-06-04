553ce473d7d5f2f7cfd0320a84d9f716ebd7aa25
Daniel Rondon <danfelrondon@gmail.com>
2023-06-03 20:27:59 -0400

    Commit 11: IntroduccionReactJS - primera-app: Agregando funcion que nos permite obtener el ganador en la aplicacion.

---
+++ IntroduccionReactJS/primera-app/src/comp/calcularGanador.js
@@ -0,0 +1,19 @@
+export default function CalculaGanador(cuadros) {
+  const lineas = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lineas.length; i++) {
+    const [a, b, c] = lineas[i];
+    if (cuadros[a] && cuadros[a] === cuadros[b] && cuadros[a] === cuadros[c]) {
+      return cuadros[a];
+    };
+  };
+  return null;
+};
