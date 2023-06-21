e4e96f4e0db407002f6e66d584faff9583bbd72a
Daniel Rondon <danfelrondon@gmail.com>
2023-06-21 17:00:19 -0400

    Commit 63: AprendiendoReactJS - IntroduccionReactJS - ReactState - ColorOrganizer: agreando Componente principal App.js y archivo index.js que ejecuta dicho componente.

---
+++ IntroduccionReactJS/react-state/color-organizer/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ColorList from "./components/ColorList";
+import AddColorForm from "./components/AddColorForm";
+
+function App() {
+  return (
+    <div>
+      <AddColorForm />
+      <ColorList />
+    </div>
+  );
+}
+
+export default App;
---
+++ IntroduccionReactJS/react-state/color-organizer/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ColorProvider from './components/ColorProvider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <ColorProvider>
+      <App />
+    </ColorProvider>
+  </React.StrictMode>
+);
