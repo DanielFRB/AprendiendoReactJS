2d487dae20600a235ddf96d87ca05728db4d9678
Daniel Rondon <danfelrondon@gmail.com>
2023-10-12 11:04:02 -0400

    Commit 202: AprendiendoReactJS - IntroduccionReactJS - ReactData - ReactVirtualizedList - UsingFetchComp: creacion de componente UserRepositories que incorpora los componentes Fetch y RepoMenu

---
+++ IntroduccionReactJS/react-data/react-virtualized-list/using-fetch-comp/src/UserRepositories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Fetch from "./Fetch";
+import RepoMenu from "./RepoMenu";
+
+export function UserRepositories({login, selectedRepo, onSelect}) {
+  return (
+    <Fetch uri={`https://api.github.com/users/${login}/repos`}
+      renderSuccess={({data}) => (
+        <RepoMenu
+          repositories={data}
+          selectedRepo={selectedRepo}
+          onSelect={onSelect}
+        />
+      )}
+    />
+  );
+}
