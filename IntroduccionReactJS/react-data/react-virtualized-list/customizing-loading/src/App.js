004b328f6cb9117cdee131c49f37d7109df7d22f
Daniel Rondon <danfelrondon@gmail.com>
2023-10-13 19:00:11 -0400

    Commit 212: AprendiendoReactJS - IntroduccionReactJS - ReactData - ReactVirtualizedList - CustomizingLoading: agregando componente principal App de la aplicacion

---
+++ IntroduccionReactJS/react-data/react-virtualized-list/customizing-loading/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Fetch from "./Fetch";
+import { UserRepositories } from "./UserRepositories";
+
+function GitHubUser({login}) {
+  return (
+    <Fetch uri={`https://api.github.com/users/${login}`}
+      renderSuccess={ UserDetails }
+    /> 
+  );
+}
+
+function UserDetails({data}) {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        { data.name && <p>{ data.name }</p> }
+        { data.location && <p>{ data.location }</p> }
+        <UserRepositories
+          login={data.login}
+          onSelect={(repoName) => console.log(`${repoName} selected`)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return <GitHubUser login="danielfrb" />
+}
