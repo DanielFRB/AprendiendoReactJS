c2685fa430b0f174faa4ae623a73219269c0269e
Daniel Rondon <danfelrondon@gmail.com>
2023-10-14 21:33:16 -0400

    Commit 223: AprendiendoReactJS - IntroduccionReactJS - ReactData - ReactVirtualizedList - ReactMarckdownApp: agregando componente principal App.js

---
+++ IntroduccionReactJS/react-data/react-virtualized-list/react-markdown-app/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { UserRepositories } from "./UserRepositories";
+import { Fetch } from "./Fetch";
+
+function UserDetails({ data }) {
+  return (
+    <div className="githubuser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+      <UserRepositories
+        login={data.login}
+        onSelect={repoName => console.log(`${repoName} selected`)}
+      />
+    </div>
+  );
+}
+
+function GitHubUser({ login }) {
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderSuccess={UserDetails}
+    />
+  );
+}
+
+export default function App() {
+  return <GitHubUser login="danielfrb" />;
+}
