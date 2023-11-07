df84bb1d33ff6cbb9f34159667dd8a1081fbd65a
Daniel Rondon <danfelrondon@gmail.com>
2023-11-07 19:47:34 -0400

    Commit 281: AprendiendoReactJS - IntroduccionReactJS - ReactRouter - CompanyWebApp: agregando App.js que renderiza las secciones de nuestra SPA a traves de React Router.

---
+++ IntroduccionReactJS/react-router/company-web-app/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { 
+  Home, About, Events, 
+  Products, Contact, Services, 
+  History, Location, Whoops404 } from './components/pages';
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route 
+          path='/' 
+          element={<Home />}
+        />
+        <Route path='about' element={<About />}>
+          <Route
+            path="services"
+            element={<Services />}
+          />
+          <Route
+            path="history"
+            element={<History />}
+          />
+          <Route
+            path="location"
+            element={<Location />}
+          />
+        </Route>
+        <Route 
+          path='events' 
+          element={<Events />} 
+        />
+        <Route 
+          path='products' 
+          element={<Products />} 
+        />
+        <Route 
+          path='contact' 
+          element={<Contact />} 
+        />
+        <Route path='*' element={<Whoops404 />} />
+      </Routes>
+    </div>
+  );
+};
